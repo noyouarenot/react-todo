986ddf07a94f1d282dfa5165376d2439275beb65
Chenghua <uguys@qq.com>
2017-07-03 18:48:30 -0700

    add filter functionality

--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -46,4 +46,37 @@ describe('TodoAPI',()=>{
         expect(actualTodos).toEqual(todos);
     });
   });
+    describe('filterTodos',()=>{
+      var todos =[{
+        id:1,
+        text:'some text here',
+        completed:true
+      },
+      {
+        id:2,
+        text:'some other text here',
+        completed:false
+      },{
+        id:3,
+        text:'here here here',
+        completed:true
+      }];
+      it('should return all items if showCompleted is true',()=>{
+        var filteredTodos = TodoAPI.filterTodos(todos,true,'');
+        expect(filteredTodos.length).toBe(3);
+      });
+      it('should return only incomplete items if showCompleted is false',()=>{
+        var filteredTodos = TodoAPI.filterTodos(todos,false,'');
+        expect(filteredTodos.length).toBe(1);
+      });
+      it('should sort by completed status',()=>{
+        var filteredTodos = TodoAPI.filterTodos(todos,true,'');
+        expect(filteredTodos[0].completed).toBe(false);
+      });
+      it('should filter todos by searchText',()=>{
+        var filteredTodos = TodoAPI.filterTodos(todos, true,'some');
+        expect(filteredTodos.length).toBe(2);
+      });
+
+    });
 });
