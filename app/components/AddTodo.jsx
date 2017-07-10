a4b05d0fb800a87b8143304f664e73ec67e03458
Chenghua <uguys@qq.com>
2017-07-09 23:33:39 -0700

    add styles

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -16,7 +16,7 @@ var AddTodo = React.createClass({
 
     render: function(){
       return (
-        <div>
+        <div className="container__footer">
           <form onSubmit={this.handleSubmit}>
           <input type="text" ref="todoText" placeholder="what to do?"></input>
           <button className="button expanded" >Add Todo</button>
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack =require('webpack');
+var path = require('path');
 
 module.exports ={
   entry:[
@@ -44,5 +45,10 @@ module.exports ={
       }
     ]
   },
+  // sassLoader:{
+  //   includePath:[
+  //     path.resolve(__dirname, './node_modules/foundation-sites/scss')
+  //   ]
+  // },
   devtool:'cheap-module-eval-source-map'
 };
